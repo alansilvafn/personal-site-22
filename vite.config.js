4ef96c2712edc0f67a8069b10f1c88e1f864c154
Alan Vieira <alanvieirads@gmail.com>
2022-08-31 10:29:29 +0100

    Performance improvements

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { splitVendorChunkPlugin } from 'vite'
+// import { splitVendorChunkPlugin } from 'vite'
+import analyze from 'rollup-plugin-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    splitVendorChunkPlugin()
-  ]
+    // splitVendorChunkPlugin()
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [analyze({
+        summaryOnly: true
+      })],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          deps: ['bootstrap', 'react-bootstrap'],
+        },
+      },
+    },
+  },
 })
